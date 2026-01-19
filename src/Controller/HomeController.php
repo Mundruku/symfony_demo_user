ea399315ecacea1f8c2c72da458594d5823f00b5
mundruku <ivanmundruku@gmail.com>
2026-01-19 13:58:30 +0300

    break points activated

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,13 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+   
+       $number = random_int(0, 100);
+       
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'number' => $number,
         ]);
     }
 }
